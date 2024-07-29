0efca00b9e4e5d5639710a38c9637c3d60ef83ff
Joeri Voet <2750496+jwavoet@users.noreply.github.com>
2024-07-29 17:02:37 +0200

    Add spot with fallback functionality

--- eventbridge.tf
+++ eventbridge.tf
@@ -13,6 +13,6 @@ resource "aws_cloudwatch_event_rule" "this" {
 }
 
 resource "aws_cloudwatch_event_target" "this" {
-  arn  = aws_lambda_function.fargate_spot_fallback.id
+  arn  = aws_lambda_function.fargate_spot_fallback.arn
   rule = aws_cloudwatch_event_rule.this.id
 }
